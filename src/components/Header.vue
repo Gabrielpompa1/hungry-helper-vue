89aa048bc6bfb1d9135ccb6715c2a9aaa96f6fb6
Gabrielpompa1 <gpompa@internet2.edu>
2023-08-14 18:40:29 -0400

    Basic header styling

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<header class="bg-blue-400 h-32 text-white">
+		<div class="h-2/3 text-4xl flex items-center px-4">
+			<h1>Hungry Helper</h1>
+		</div>
+		<nav class="bg-white border-y border-blue-400 h-1/3 flex px-4">
+			<ul class="flex text-blue-400">
+				<li
+					class="hover:bg-blue-400 hover:text-white h-full flex items-center p-2"
+				>
+					<a href="#">Home</a>
+				</li>
+				<li
+					class="hover:bg-blue-400 hover:text-white h-full flex items-center p-2"
+				>
+					<a href="#">Random</a>
+				</li>
+				<li
+					class="hover:bg-blue-400 hover:text-white h-full flex items-center p-2"
+				>
+					<a href="#">Categories</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
